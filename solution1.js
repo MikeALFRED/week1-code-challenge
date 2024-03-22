567286ddd1aa9e1ee65ddb28a050afe4a424863d
Paps <Mikealfredmuigai@gmail.com>
2024-03-22 17:15:44 +0300

    complete code

---
+++ solution1.js
@@ -0,0 +1,25 @@
+Input = ("enter students marks:")
+//converting marks to a floating -point number
+ let marks = parseFloat(Input)
+ 
+//checking if marks is between 0 and 100 by using the AND Operator
+if(marks>=0 && marks<=100){
+    let grade
+
+    if (marks > 79) {
+        grade ='A'  
+    }else if (marks >=60 && marks<=79) {
+        grade = 'B'
+    } else if (marks >=49 && marks<=59) {
+        grade = 'C'   
+    } else if (marks >=40 && marks<=49) {
+        grade ='D'   
+    } else {
+        grade = 'E' 
+    }
+    // Output the grade
+        
+    console.log ( grade)
+} else{
+   console.log('!!!!Marks should be between 0 and 100' )
+}

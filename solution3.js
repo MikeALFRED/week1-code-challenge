2546cfc3538d28e76fe0a36909f3475ab51837d9
Paps <Mikealfredmuigai@gmail.com>
2024-03-22 19:46:07 +0300

    complete lab

---
+++ solution3.js
@@ -0,0 +1,38 @@
+function calculateNetSalary(basicSalary,benefits){
+    let taxRate = 0.25
+    let nhifRate = 0.05
+    let nssfRate = 0.06
+
+    // calculating the grosssalary,payee,nhifdeductions,nssfdeductions,netsalary
+
+     let grossSalary = basicSalary + benefits
+     let payee = grossSalary * taxRate
+     let nhifDeductions = grossSalary * nhifRate
+     let nssfDeductions = grossSalary * nssfRate
+     let netSalary = grossSalary - payee - nhifDeductions - nssfDeductions
+
+// outputting the results by logging them
+
+     console.log("Gross salary:" + grossSalary)
+     console.log("Payee:" + payee)
+     console.log("NHIF deductions:" + nhifDeductions)
+     console.log("NSSF deductions:" + nssfDeductions)
+     console.log("Net salary:" + netSalary)
+
+}
+
+Input = ("Enter basic salary")
+
+// converting Input to a floating number by using parseFloat
+
+let basicSalary = parseFloat(Input)
+
+input = ("Enter benefits")
+
+// converting input to a floating number by using parseFloat
+
+let benefits = parseFloat(input)
+
+// function call
+
+calculateNetSalary(basicSalary,benefits)
